8055a3d53212fdd6a9b50a9965e9afaad6df94c5
kidGodzilla <futhey@gmail.com>
2020-11-15 15:16:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 // Process-level
 const cluster = require('cluster');
-const os = require('os');
-
-
+// const os = require('os');
 
 /**
  * Run a cluster of CPUs * 2 "threads", which can die and restart
  */
 if (cluster.isMaster) {
-    var cpuCount = os.cpus().length;
+    // let cpuCount = os.cpus().length;
+    let clusterCount = 1;
 
-    var clusterCount = cpuCount * 2;
+    for (let i = 0; i < clusterCount; i += 1) { cluster.fork() }
 
-    clusterCount = 1;
-
-    for (var i = 0; i < clusterCount; i += 1) {
-        cluster.fork();
-    }
-
-    cluster.on('exit', function (worker) {
-        console.log('Worker %d died :(', worker.id); // Replace the dead worker, we're not sentimental
+    cluster.on('exit', (worker) => {
+        console.log('Worker %d died', worker.id); // Replace the dead worker, we're not sentimental
         cluster.fork();
     });
 
